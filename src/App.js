28408a7d2598a212bb951d4752287596cc707ebd
nishu-kumar599 <lapvipul123@gmail.com>
2022-11-15 15:14:11 +0530

    new-changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import "./App.css";
-import "./Component/SignUp/Signup.css";
-import "./Component/Pages/LoginPage.css";
-import "./Component/Login/Login.css";
-import "./Component/Product/Product.css";
-import "./Component/Dashboard/Dashboard.css";
 import { Routes, Route } from "react-router-dom";
-import LoginPage from "./Component/Pages/LoginPage";
 import Dashboard from "./Component/Dashboard/Dashboard";
 import ProductDetail from "./Component/Product/ProductDetail";
-import Product from "./Component/Product/Product";
+import Header from "./Component/Header/Header";
+import AddCart from "./Component/Context/AddCart";
 
 function App() {
   return (
-    // <Routes>
-    // <Route path='/' element={<LoginPage/>}/>
-    // <Route path='/' element={<Dashboard/>}/>
-    // </Routes>
-    // <Dashboard />
-    <Routes>
-      <Route path="/:id" element={<Dashboard />} />
-      <Route path="/Product/:id" element={<ProductDetail />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/:id" element={<Dashboard />} />
+        <Route path="/Product/:id" element={<ProductDetail />} />
+        <Route path="/Product/AddCart" element={<AddCart />} />
+      </Routes>
+    </>
   );
 }
 
